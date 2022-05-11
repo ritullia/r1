2d2457b129186e52c2600a7eb3b61033c5a7745f
Rita <94860684+ritullia@users.noreply.github.com>
2022-05-11 20:42:50 +0300

    sort.size

--- src/Components/003/Rainbow.jsx
+++ src/Components/003/Rainbow.jsx
@@ -6,7 +6,7 @@ function Rainbow(props){
 
         {
 
-        props.colors.map((c, i) => <div className='colors' key={i} style={
+        props.colors.sort((a, b) => b.size - a.size).map((c, i) => <div className='colors' key={i} style={
             {
                 backgroundColor: c.color,
                 height: c.size
